8238309e4c9bd8b554719f8fed5886f0f5a63742
Sanket01-debug <sanketkansal2001@gmail.com>
2026-03-24 20:59:46 +0530

    feat:Refactor App.js to use JSX for rendering and update index.html structure

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div", { id: "parent"}, [
-    React.createElement("div", {id: "child"},[
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", {id: "child2"},[
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-]);
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX 🚀
+    </h1>
+);
 
-console.log(parent);
+// React Functional Component
+const HeadingComponent = () => (
+    <div id="container">
+        {Title()}
+        <Title></Title>
+        <Title/>
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
