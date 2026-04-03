509e4f5e93cf7b6c4ecaa754e8a08957d826aa1c
Sanket01-debug <sanketkansal2001@gmail.com>
2026-04-03 17:15:57 +0530

    fix: Update image sources in Header and RestaurantCard components to use constants

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constants";
+
 const RestaurantCard = (props) => {
     const { resData } = props;
 
@@ -14,7 +16,7 @@ const RestaurantCard = (props) => {
         <div className="res-card" style={{
             backgroundColor: "#f0f0f0"
         }}>
-            <img className="res-logo" alt="res-logo" src={+ cloudinaryImageId} />
+            <img className="res-logo" alt="res-logo" src={CDN_URL+ cloudinaryImageId} />
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} stars</h4>
